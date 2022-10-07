2f5c5480c9d878241d10885f3e9b1ae57ad7a9f8
NireepV <109134856+NireepV@users.noreply.github.com>
2022-10-07 15:28:24 +1030

    removed river and road files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <string>
-//#include "Mechanics/Player.cpp"
-//#include "Mechanics/Headers/Road.h"
-//#include "Mechanics/Headers/River.h"
+#include "Mechanics/Player.cpp"
 #include "Mechanics/Headers/MapGen.h"
 #include "Global.cpp"
 
@@ -11,16 +9,12 @@ class OOPGame
 private:
     sf::RenderWindow *win;
     MapGen map;
-    //Player* frog;
-    //River* river;
-    //River* road;
+    Player* frog;
 public:
     OOPGame(int size, std::string Title){
         win = new sf::RenderWindow(sf::VideoMode(size, size), Title);
         MapGen map;
-        //frog = new Player;
-        //river = new River;
-        //road = new River;
+        frog = new Player;
     }
     void run()
     {
@@ -38,17 +32,13 @@ public:
             win->clear(sf::Color(0,95,86));
             map.load("Map.png", sf::Vector2u(64, 64), level, 16, 15);
             win->draw(map);
-            //road->draw(win);
-            //river->draw(win);
-            //frog->draw(win);
+            frog->draw(win);
             win->display();
         }
     }
     ~OOPGame(){
         delete win;
-        //delete frog;
-        //delete road;
-        //delete river;
+        delete frog;
     }
 };
 
